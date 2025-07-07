1da714718e97642661faefc94e4da5e61a54363f
Shefat-Shohan <shohan.ks3@gmail.com>
2025-07-07 14:51:31 +0600

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import LoginForm from "./catalog_components/LoginForm";
 
 export default function Home() {
   return (
-      <div className="max-w-7xl mx-auto lg:py-20 lg:px-4 md:px-6 px-4 py-10">
-        <Catalog />
-      </div>
+    <div className="max-w-7xl mx-auto lg:py-20 lg:px-4 md:px-6 px-4 py-10">
+      <Catalog />
+    </div>
   );
 }
