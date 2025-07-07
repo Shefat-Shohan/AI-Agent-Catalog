7693338ebee0c7330010c3f3fa4be37b894db867
Shefat-Shohan <shohan.ks3@gmail.com>
2025-07-07 15:07:31 +0600

    fix error

--- src/app/catalog_components/Catalog.tsx
+++ src/app/catalog_components/Catalog.tsx
@@ -1,7 +1,8 @@
 import AgentSearch from "./AgentSerch";
 
 export default async function Catalog() {
-  const res = await fetch("/api/agents", {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
+  const res = await fetch(`${baseUrl}/api/agents`, {
     cache: "no-cache",
   });
   const agents = await res.json();
