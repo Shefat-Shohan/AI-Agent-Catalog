7fd3955ad7f62084254e4adfe2f3b129973084f1
Shefat-Shohan <shohan.ks3@gmail.com>
2025-07-07 15:25:33 +0600

    fix error

--- src/app/catalog_components/Catalog.tsx
+++ src/app/catalog_components/Catalog.tsx
@@ -2,12 +2,16 @@ import AgentSearch from "./AgentSerch";
 
 export default async function Catalog() {
   const isProd = process.env.NODE_ENV === "production";
-  const baseUrl = isProd ? "" : process.env.NEXT_PUBLIC_BASE_URL;
+  const baseUrl = isProd
+    ? process.env.NEXT_PUBLIC_BASE_URL ??
+      "https://ai-agent-catalog-six.vercel.app/"
+    : "http://localhost:3000";
 
   const res = await fetch(`${baseUrl}/api/agents`, {
     cache: "no-cache",
   });
 
   const agents = await res.json();
+
   return <AgentSearch agents={agents} />;
 }
