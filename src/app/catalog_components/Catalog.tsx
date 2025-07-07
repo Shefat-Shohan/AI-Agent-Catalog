414ac518945c10e513bde8e2a64f02d149cb14c1
Shefat-Shohan <shohan.ks3@gmail.com>
2025-07-07 15:31:30 +0600

    fix error

--- src/app/catalog_components/Catalog.tsx
+++ src/app/catalog_components/Catalog.tsx
@@ -1,17 +1,12 @@
 import AgentSearch from "./AgentSerch";
 
 export default async function Catalog() {
-  const isProd = process.env.NODE_ENV === "production";
-  const baseUrl = isProd
-    ? process.env.NEXT_PUBLIC_BASE_URL ??
-      "https://ai-agent-catalog-six.vercel.app/"
-    : "http://localhost:3000";
-
-  const res = await fetch(`${baseUrl}/api/agents`, {
-    cache: "no-cache",
-  });
-
+  const res = await fetch(
+    "https://686b9342e559eba908732398.mockapi.io/aiagentCatalog",
+    {
+      cache: "no-cache",
+    }
+  );
   const agents = await res.json();
-
   return <AgentSearch agents={agents} />;
 }
